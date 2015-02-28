8c85564da07844062d75cc20b0f5644ab7fc5469
Richard Hopkins <richard.hopkins@xamarin.com>
2015-02-28 18:38:50 +0000

    Main files added to project for moving to Github

---
+++ GoogleMACTRBindingUnified/ApiDefinition.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Drawing;
+
+using ObjCRuntime;
+using Foundation;
+using UIKit;
+
+namespace GoogleMACTRBindingUnified
+{
+	// @interface ACTReporter : NSObject
+	[BaseType (typeof (NSObject))]
+	interface ACTReporter {
+
+		// +(NSString *)SDKVersion;
+		[Static, Export ("SDKVersion")]
+		string SDKVersion ();
+
+		// -(BOOL)report;
+		[Export ("report")]
+		bool Report ();
+	}
+
+	// @interface ACTConversionReporter : ACTReporter
+	[BaseType (typeof (ACTReporter))]
+	interface ACTConversionReporter {
+
+		// -(instancetype)initWithConversionID:(NSString *)conversionID label:(NSString *)label value:(NSString *)value isRepeatable:(BOOL)isRepeatable;
+		[Export ("initWithConversionID:label:value:isRepeatable:")]
+		IntPtr Constructor (string conversionID, string label, string value, bool isRepeatable);
+
+		// -(instancetype)initWithProductID:(NSString *)productID value:(NSString *)value isRepeatable:(BOOL)isRepeatable;
+		[Export ("initWithProductID:value:isRepeatable:")]
+		IntPtr Constructor (string productID, string value, bool isRepeatable);
+
+		// @property (copy, nonatomic) NSString * value;
+		[Export ("value")]
+		string Value { get; set; }
+
+		// +(void)reportWithConversionID:(NSString *)conversionID label:(NSString *)label value:(NSString *)value isRepeatable:(BOOL)isRepeatable;
+		[Static, Export ("reportWithConversionID:label:value:isRepeatable:")]
+		void ReportWithConversionID (string conversionID, string label, string value, bool isRepeatable);
+
+		// +(void)reportWithProductID:(NSString *)productID value:(NSString *)value isRepeatable:(BOOL)isRepeatable;
+		[Static, Export ("reportWithProductID:value:isRepeatable:")]
+		void ReportWithProductID (string productID, string value, bool isRepeatable);
+
+		// +(BOOL)registerReferrer:(NSURL *)clickURL;
+		[Static, Export ("registerReferrer:")]
+		bool RegisterReferrer (NSUrl clickURL);
+	}
+
+	// @interface ACTRemarketingReporter : ACTReporter
+	[BaseType (typeof (ACTReporter))]
+	interface ACTRemarketingReporter {
+
+		// -(instancetype)initWithConversionID:(NSString *)conversionID customParameters:(NSDictionary *)customParameters;
+		[Export ("initWithConversionID:customParameters:")]
+		IntPtr Constructor (string conversionID, NSDictionary customParameters);
+
+		// +(void)reportWithConversionID:(NSString *)conversionID customParameters:(NSDictionary *)customParameters;
+		[Static, Export ("reportWithConversionID:customParameters:")]
+		void ReportWithConversionID (string conversionID, NSDictionary customParameters);
+	}
+
+	// @interface ACTAutomatedUsageTracker : NSObject
+	[BaseType (typeof (NSObject))]
+	interface ACTAutomatedUsageTracker {
+
+		// +(void)enableAutomatedUsageReportingWithConversionID:(NSString *)conversionID;
+		[Static, Export ("enableAutomatedUsageReportingWithConversionID:")]
+		void EnableAutomatedUsageReportingWithConversionID (string conversionID);
+
+		// +(void)disableAutomatedUsageReportingWithConversionID:(NSString *)conversionID;
+		[Static, Export ("disableAutomatedUsageReportingWithConversionID:")]
+		void DisableAutomatedUsageReportingWithConversionID (string conversionID);
+	}
+
+}
+
